5e84f5885dab932a4179f77fbe3cc451aa22e2b9
huangpeng <2846901258@qq.com>
2016-10-18 15:27:22 +0800

    init

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <?php
 
-define('APPLICATION_PATH', dirname(__FILE__));
-
-$application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
-
-$application->bootstrap()->run();
+if(isset($_GET['debug']) && 1 == $_GET['debug']){
+    ini_set('display_errors', true);
+    error_reporting(E_ALL);
+}
+date_default_timezone_set('Asia/Shanghai');
+define('DS', DIRECTORY_SEPARATOR);
+define('APP_NAME', basename(__DIR__));
+define('ROOT_DIR', dirname(dirname(__DIR__)));
+define('CONF_DIR', ROOT_DIR . DS . 'conf');
+define('APP_DIR', ROOT_DIR . DS . 'apps' . DS . APP_NAME);
+define('LOG_DIR', ROOT_DIR . DS . 'logs');
+define('APP_CONF_DIR', CONF_DIR . DS . APP_NAME);
+define('UPLOAD_DIR', ROOT_DIR . DS . 'webroot/static');
+try{
+    $app = new Yaf_Application(APP_CONF_DIR . DS . 'application.ini');
+    $app->bootstrap()->run();
+}catch(Exception $e){
+    Ald_Log::warning('%s: exception occured: %s[%s]', $e->getMessage(), $e->getCode());
+}
+$cost = microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'];
+Ald_Log::notice(sprintf('cost[%s]', $cost), true);
 ?>
